368f1fecb467445e25939058acc760571c05ef05
colej <johnston.ccj@gmail.com>
2019-02-21 14:00:35 +0100

    f

--- mesa/grid_io_support.py
+++ mesa/grid_io_support.py
@@ -172,7 +172,7 @@ def make_sobol_filenames(adjpars, summary_file='./grid.summary', inlist_prefix=N
     directory_segments = []
     keys               = adjpars.keys()
     indices            = [ adjpars[key]['index'] for key in keys ]
-    zipped             = sorted(zip(keys,indices),key=lambda x: x[1]))
+    zipped             = sorted(zip(keys,indices),key=lambda x: x[1])
     sortkeys,_         = zip(*zipped)
     # sortkeys           = np.array()
 
